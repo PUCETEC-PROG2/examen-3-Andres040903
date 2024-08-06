90396d181a1f7c4125878535b95390c485088a11
Andres040903 <eacaillagua@puce.edu.ec>
2024-08-05 21:46:02 -0500

    Examen

--- album_manager/models.py
+++ album_manager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    country = models.CharField(max_length=30, null=False)
+    picture = models.ImageField(upload_to='artist_images')  
+
+    def __str__(self) -> str:
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=30, null=False)
+    release_year = models.IntegerField(null=False)
+    genre = models.CharField(max_length=30, null=False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='album_images') 
+
+    def __str__(self) -> str:
+        return self.title
+
